aa048a62c67a0784e80a85450fa9c603169ebad9
Jonny Bergdahl <bergdahl@users.noreply.github.com>
2024-06-07 14:08:29 +0200

    Update JBWOPR docs

--- jbwopr/search/all_14.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['web_5fportal_5fpassword_0',['WEB_PORTAL_PASSWORD',['../class_j_b_wopr_wi_fi_device.html#a544d97c4c58477bda4d308b8861b2b3e',1,'JBWoprWiFiDevice']]],
-  ['webportalstart_1',['webPortalStart',['../group___wi_fi_group.html#gac9254fb9737ede31c5042336de686744',1,'JBWoprWiFiDevice']]],
-  ['webportalstop_2',['webPortalStop',['../group___wi_fi_group.html#ga5f456c865b9dd548e8699a8779c8b403',1,'JBWoprWiFiDevice']]],
-  ['wifi_20related_20methods_3',['WiFi related methods',['../group___wi_fi_group.html',1,'']]],
-  ['work_20in_20progress_4',['Work in progress',['../index.html#autotoc_md1',1,'']]]
-];
